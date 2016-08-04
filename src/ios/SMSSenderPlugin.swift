c64e92c65ca632bfbeccfc07c36cfed9beba98cd
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-08-04 20:47:48 +0800

    test

--- src/ios/SMSSenderPlugin.swift
+++ src/ios/SMSSenderPlugin.swift
@@ -1,10 +1,21 @@
+import UIKit
+import MessageUI
 
 @available(iOS 8.0, *)
 @objc(SMSSenderPlugin) class SMSSenderPlugin : CDVPlugin {
     func sendMessage(command: CDVInvokedUrlCommand) {
         var pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_ERROR
+            status: CDVCommandStatus_OK
         )
+
+        let mobile_number = command.arguments[0] as? String ?? ""
+        let message = command.arguments[1] as? String ?? ""
+
+        let messageVC = MFMessageComposeViewController()
+        messageVC.body = message
+        messageVC.recipients = [mobile_number]
+
+        viewController.presentViewController(messageVC, animated: true, completion: nil)
 
         self.commandDelegate!.sendPluginResult(
             pluginResult, 
