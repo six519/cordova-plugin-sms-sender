1d0339958606704772edab457379dd8a31082af7
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-08-05 09:09:34 +0800

    add protocol delegate

--- src/ios/SMSSenderPlugin.swift
+++ src/ios/SMSSenderPlugin.swift
@@ -2,7 +2,7 @@ import UIKit
 import MessageUI
 
 @available(iOS 8.0, *)
-@objc(SMSSenderPlugin) class SMSSenderPlugin : CDVPlugin {
+@objc(SMSSenderPlugin) class SMSSenderPlugin : CDVPlugin, MFMessageComposeViewControllerDelegate {
     func sendMessage(command: CDVInvokedUrlCommand) {
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_OK
@@ -11,15 +11,31 @@ import MessageUI
         let mobile_number = command.arguments[0] as? String ?? ""
         let message = command.arguments[1] as? String ?? ""
 
-        let messageVC = MFMessageComposeViewController()
-        messageVC.body = message
-        messageVC.recipients = [mobile_number]
+        let messageCVC = MFMessageComposeViewController()
+        messageCVC.body = message
+        messageCVC.recipients = [mobile_number]
+        messageCVC.messageComposeDelegate = self
 
-        viewController.presentViewController(messageVC, animated: true, completion: nil)
+        viewController.presentViewController(messageCVC, animated: true, completion: nil)
 
         self.commandDelegate!.sendPluginResult(
             pluginResult, 
             callbackId: command.callbackId
         )
     }
+
+    func messageComposeViewController(controller: MFMessageComposeViewController, didFinishWithResult result: MessageComposeResult) {
+        switch result.rawValue {
+            case MessageComposeResultCancelled.rawValue :
+                //Canceled
+            case MessageComposeResultFailed.rawValue :
+                //Failed
+            case MessageComposeResultSent.rawValue :
+                //Message Sent
+            default:
+                break
+        }
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+
 }
