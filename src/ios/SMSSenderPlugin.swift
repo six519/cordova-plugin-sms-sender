8805bbd4167b65f23f93d49dc5d07ea79999d085
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-08-06 18:05:13 +0800

    move the callback to messageComposeViewController delegate

--- src/ios/SMSSenderPlugin.swift
+++ src/ios/SMSSenderPlugin.swift
@@ -3,41 +3,54 @@ import MessageUI
 
 @available(iOS 8.0, *)
 @objc(SMSSenderPlugin) class SMSSenderPlugin : CDVPlugin, MFMessageComposeViewControllerDelegate {
+    var thisCommand: CDVInvokedUrlCommand?
+    
     func sendMessage(command: CDVInvokedUrlCommand) {
-        var pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_OK
-        )
 
         let mobile_number = command.arguments[0] as? String ?? ""
         let message = command.arguments[1] as? String ?? ""
-
         let messageCVC = MFMessageComposeViewController()
         messageCVC.body = message
         messageCVC.recipients = [mobile_number]
         messageCVC.messageComposeDelegate = self
-
+        thisCommand = command
         viewController.presentViewController(messageCVC, animated: true, completion: nil)
-
-        self.commandDelegate!.sendPluginResult(
-            pluginResult, 
-            callbackId: command.callbackId
-        )
     }
 
     func messageComposeViewController(controller: MFMessageComposeViewController, didFinishWithResult result: MessageComposeResult) {
+        
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK
+        )
+
         switch result.rawValue {
             case MessageComposeResultCancelled.rawValue :
                 //Canceled
-                print("Cancelled!")
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_OK,
+                    messageAsString: "Canceled"
+                )
             case MessageComposeResultFailed.rawValue :
                 //Failed
-                print("Failed")
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_ERROR,
+                    messageAsString: "Failed"
+                )
             case MessageComposeResultSent.rawValue :
                 //Message Sent
-                print("Sent")
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_OK,
+                    messageAsString: "Sent"
+                )
             default:
                 break
         }
+
+        self.commandDelegate!.sendPluginResult(
+            pluginResult, 
+            callbackId: thisCommand!.callbackId
+        )
+
         controller.dismissViewControllerAnimated(true, completion: nil)
     }
 
